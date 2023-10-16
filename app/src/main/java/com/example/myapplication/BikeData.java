f7f8536ea53c5975cb821f46c1e9bba241585ee9
Patryk Malinowski <patryk.malinowski@mycit.ie>
2023-10-16 17:03:14 +0100

    Added bike and other datasets into their respective files - BikeData.java and OtherData.java

---
+++ app/src/main/java/com/example/myapplication/BikeData.java
@@ -0,0 +1,104 @@
+//Patryk Malinowski
+//R00210173
+//Garage App Project
+
+package com.example.myapplication;
+
+import java.util.ArrayList;
+
+public class BikeData {
+    public static ArrayList<String> bikeTitles = new ArrayList<>();
+    public static ArrayList<String> bikeYear = new ArrayList<>();
+    public static ArrayList<Integer> bikePrice = new ArrayList<>();
+    public static ArrayList<Integer> bikeImage = new ArrayList<>();
+    public static ArrayList<String> bikeUrl = new ArrayList<>();
+    public static void loadBikeData() {
+
+
+        // data for bike titles
+        bikeTitles.add("");
+        bikeTitles.add("");
+        bikeTitles.add("");
+        bikeTitles.add("");
+        bikeTitles.add("");
+        bikeTitles.add("");
+        bikeTitles.add("");
+        bikeTitles.add("");
+        bikeTitles.add("");
+        bikeTitles.add("");
+        bikeTitles.add("");
+        bikeTitles.add("");
+        bikeTitles.add("");
+        bikeTitles.add("");
+        bikeTitles.add("");
+
+        // data for bike years
+        bikeYear.add("2000");
+        bikeYear.add("2000");
+        bikeYear.add("2000");
+        bikeYear.add("2000");
+        bikeYear.add("2000");
+        bikeYear.add("2000");
+        bikeYear.add("2000");
+        bikeYear.add("2000");
+        bikeYear.add("2000");
+        bikeYear.add("2000");
+        bikeYear.add("2000");
+        bikeYear.add("2000");
+        bikeYear.add("2000");
+        bikeYear.add("2000");
+        bikeYear.add("2000");
+
+        // data for bike price
+        bikePrice.add(0);
+        bikePrice.add(0);
+        bikePrice.add(0);
+        bikePrice.add(2);
+        bikePrice.add(0);
+        bikePrice.add(0);
+        bikePrice.add(0);
+        bikePrice.add(0);
+        bikePrice.add(0);
+        bikePrice.add(0);
+        bikePrice.add(0);
+        bikePrice.add(0);
+        bikePrice.add(0);
+        bikePrice.add(0);
+        bikePrice.add(0);
+
+        // data for bike images
+        bikeImage.add(R.drawable.default_car_image);
+        bikeImage.add(R.drawable.default_car_image);
+        bikeImage.add(R.drawable.default_car_image);
+        bikeImage.add(R.drawable.default_car_image);
+        bikeImage.add(R.drawable.default_car_image);
+        bikeImage.add(R.drawable.default_car_image);
+        bikeImage.add(R.drawable.default_car_image);
+        bikeImage.add(R.drawable.default_car_image);
+        bikeImage.add(R.drawable.default_car_image);
+        bikeImage.add(R.drawable.default_car_image);
+        bikeImage.add(R.drawable.default_car_image);
+        bikeImage.add(R.drawable.default_car_image);
+        bikeImage.add(R.drawable.default_car_image);
+        bikeImage.add(R.drawable.default_car_image);
+        bikeImage.add(R.drawable.default_car_image);
+
+        // data for bike URLs
+        bikeUrl.add("");
+        bikeUrl.add("");
+        bikeUrl.add("");
+        bikeUrl.add("");
+        bikeUrl.add("");
+        bikeUrl.add("");
+        bikeUrl.add("");
+        bikeUrl.add("");
+        bikeUrl.add("");
+        bikeUrl.add("");
+        bikeUrl.add("");
+        bikeUrl.add("");
+        bikeUrl.add("");
+        bikeUrl.add("");
+        bikeUrl.add("");
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/OtherData.java
@@ -0,0 +1,87 @@
+//Patryk Malinowski
+//R00210173
+//Garage App Project
+
+package com.example.myapplication;
+
+import java.util.ArrayList;
+
+public class OtherData {
+    public static ArrayList<String> otherTitles = new ArrayList<>();
+    public static ArrayList<Integer> otherPrice = new ArrayList<>();
+    public static ArrayList<Integer> otherImage = new ArrayList<>();
+    public static ArrayList<String> otherUrl = new ArrayList<>();
+    public static void loadOtherData() {
+
+
+        // data for other titles
+        otherTitles.add("");
+        otherTitles.add("");
+        otherTitles.add("");
+        otherTitles.add("");
+        otherTitles.add("");
+        otherTitles.add("");
+        otherTitles.add("");
+        otherTitles.add("");
+        otherTitles.add("");
+        otherTitles.add("");
+        otherTitles.add("");
+        otherTitles.add("");
+        otherTitles.add("");
+        otherTitles.add("");
+        otherTitles.add("");
+
+
+        // data for other price
+        otherPrice.add(0);
+        otherPrice.add(0);
+        otherPrice.add(0);
+        otherPrice.add(2);
+        otherPrice.add(0);
+        otherPrice.add(0);
+        otherPrice.add(0);
+        otherPrice.add(0);
+        otherPrice.add(0);
+        otherPrice.add(0);
+        otherPrice.add(0);
+        otherPrice.add(0);
+        otherPrice.add(0);
+        otherPrice.add(0);
+        otherPrice.add(0);
+
+        // data for other images
+        otherImage.add(R.drawable.default_car_image);
+        otherImage.add(R.drawable.default_car_image);
+        otherImage.add(R.drawable.default_car_image);
+        otherImage.add(R.drawable.default_car_image);
+        otherImage.add(R.drawable.default_car_image);
+        otherImage.add(R.drawable.default_car_image);
+        otherImage.add(R.drawable.default_car_image);
+        otherImage.add(R.drawable.default_car_image);
+        otherImage.add(R.drawable.default_car_image);
+        otherImage.add(R.drawable.default_car_image);
+        otherImage.add(R.drawable.default_car_image);
+        otherImage.add(R.drawable.default_car_image);
+        otherImage.add(R.drawable.default_car_image);
+        otherImage.add(R.drawable.default_car_image);
+        otherImage.add(R.drawable.default_car_image);
+
+        // data for other URLs
+        otherUrl.add("");
+        otherUrl.add("");
+        otherUrl.add("");
+        otherUrl.add("");
+        otherUrl.add("");
+        otherUrl.add("");
+        otherUrl.add("");
+        otherUrl.add("");
+        otherUrl.add("");
+        otherUrl.add("");
+        otherUrl.add("");
+        otherUrl.add("");
+        otherUrl.add("");
+        otherUrl.add("");
+        otherUrl.add("");
+    }
+
+}
